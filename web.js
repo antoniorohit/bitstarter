81169a399400b70ad881a974b9b811207d8f89d6
Ubuntu <ubuntu@ip-172-31-39-59.us-west-2.compute.internal>
2013-07-12 05:38:08 +0000

    Tried correcting application error again

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 
  var buffer = new Buffer("Hello from websomething majijigy");
  buffer = fs.readFileSync('index.html');
-  response.send(buffer.ToString());
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
