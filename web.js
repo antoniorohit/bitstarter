41fcaab7d57cc007f94f4f01da35491c6f753806
Ubuntu <ubuntu@ip-172-31-39-59.us-west-2.compute.internal>
2013-07-12 05:41:04 +0000

    Tried correcting application error again

--- web.js
+++ web.js
@@ -4,10 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
- var buffer = new Buffer("Hello from websomething majijigy");
- buffer = fs.readFileSync('index.html');
-  response.send(buffer);
+ 
+ var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
