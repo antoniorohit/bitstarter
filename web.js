68041b05f2b410928bdec765e7210f0e893231d0
Ubuntu <ubuntu@ip-172-31-39-59.us-west-2.compute.internal>
2013-07-12 05:30:03 +0000

    Tried correcting application error again

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
  var buffer = new Buffer("Hello from websomething majijigy");
- buffer = fs.readFile("index.html");
+ buffer = fs.readFile("$HOME//bitstarter//index.html");
  var string = (buffer.ToString());
   response.send(string);
 });
